0143dad8d383a981ad7b86b1cd06765d10cfb3b4
Tanvir-Hasan-009 <132391394+Tanvir-Hasan-009@users.noreply.github.com>
2023-09-18 20:36:06 +0900

    Add files via upload

---
+++ tanvirweek2.2.py
@@ -0,0 +1,5 @@
+
+C = input("Input the temperature in Celsius:")
+c = float(C)
+fahrenhit=(c*9/5) + 32
+print("The temperature in Fahrenheit is:",fahrenhit)
---
+++ tanvirweek2.3.py
@@ -0,0 +1,5 @@
+Hour= input("Enter Hour:\n")
+Hour= float(Hour)
+Rate=input("Enter rate: \n")
+Rate=float(Rate)
+print("The Salary is:",Hour*Rate)
